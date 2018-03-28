2796df9db0348f672b8b11e4f252b96f8b1d23f9
alyxandriascott <alyxan.hope26@gmail.com>
2018-03-28 11:53:28 -0500

    added notes to drills for future reference

--- src/main/java/guessServlet.java
+++ src/main/java/guessServlet.java
@@ -5,17 +5,22 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.Random;
-
+//annotation allows us to define this class as a servlet
 @WebServlet(name = "guessServlet", urlPatterns = "/guess")
+//the above urlPatterns tell servlet what to look for from the jsp(s)
 public class guessServlet extends HttpServlet {
 
     protected Random rand = new Random();
     protected int randNum = rand.nextInt(100);
+//    random num generated and assigned outside the scope of the doPost method so there isnt a new random number
+//    generated every time a post req. is made
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         req.getRequestDispatcher("/guess.jsp").forward(req, resp);
     }
+//    above doGet allows us to define the urlPattern in line 9 as just "/guess" vs. "/guess.jsp". this allows us to
+//    hide the actual url pattern and type
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
@@ -29,7 +34,7 @@ public class guessServlet extends HttpServlet {
         } else {
             req.setAttribute("guess", "Correct!");
         }
-
+//the below line will actually send the info back to the result jsp page, based on which parameter it falls under.
         req.getRequestDispatcher("/result.jsp").forward(req, resp);
     }
 }
