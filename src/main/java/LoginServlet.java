00233016972dc338022e1c7883b2a50c9fb3a7c7
alyxandriascott <alyxan.hope26@gmail.com>
2018-03-29 14:08:50 -0500

    session cookie exercise w/ notations

--- src/main/java/LoginServlet.java
+++ src/main/java/LoginServlet.java
@@ -3,6 +3,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 @WebServlet(name = "LoginServlet", urlPatterns = "/login")
@@ -12,11 +13,18 @@ public class LoginServlet extends HttpServlet {
     }
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+
         String username = request.getParameter("username");
         String password = request.getParameter("password");
+//below code: session is the variable that is grabbing the current session
+        HttpSession session = request.getSession();
+
         boolean validAttempt = username.equals("admin") && password.equals("password");
 
         if (validAttempt) {
+//            below code: assigns the user parameter to the username variable, which is passed onto the jsp via the session
+//            only terminated when told to or when the browser is closed
+            session.setAttribute("user", username);
             response.sendRedirect("/profile");
         } else {
             response.sendRedirect("/login");
--- src/main/webapp/partials/navbar.jsp
+++ src/main/webapp/partials/navbar.jsp
@@ -1,3 +1,4 @@
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 <nav class="navbar navbar-default">
     <div class="container-fluid">
         <!-- Brand and toggle get grouped for better mobile display -->
@@ -5,7 +6,13 @@
             <a class="navbar-brand" href="/ads">Adlister</a>
         </div>
         <ul class="nav navbar-nav navbar-right">
+            <%--below code: determines login/logout status based on user status as defined in login/logout servlets--%>
+        <c:if test="${not empty user}">
+            <li><a href="/logout">Log Out</a></li>
+        </c:if>
+        <c:if test="${empty user}">
             <li><a href="/login">Login</a></li>
+        </c:if>
         </ul>
     </div><!-- /.navbar-collapse -->
     </div><!-- /.container-fluid -->
